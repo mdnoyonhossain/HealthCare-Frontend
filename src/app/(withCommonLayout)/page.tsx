58a5b468328291202b73595003967c1f851ef1a4
Noyon Hossain <noyonhossain560@gmail.com>
2025-05-09 21:25:19 +0600

    Patient Testimonials Running

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -4,6 +4,7 @@ import Specialties from "@/components/UI/HomePage/Specialties/Specialties";
 import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
 import WhyChooseUs from "../../components/UI/HomePage/WhyChooseUs/WhyChooseUs";
 import HowItWork from "@/components/UI/HomePage/HowItWork/HowItWork";
+import Testimonials from "@/components/UI/HomePage/Testimonials/Testimonials";
 
 const HomePage = () => {
     return (
@@ -14,6 +15,7 @@ const HomePage = () => {
             <TopRatedDoctors />
             <WhyChooseUs />
             <HowItWork />
+            <Testimonials />
         </>
     );
 };
---
+++ src/components/UI/HomePage/Testimonials/Testimonials.tsx
@@ -0,0 +1,19 @@
+const Testimonials = () => {
+    return (
+        <section className="bg-[#EFF6FF] px-4 py-10 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto">
+                <div className="text-center mb-12">
+                    <h2 className="text-4xl font-bold text-gray-900">Patient Testimonials</h2>
+                    <p className="mt-4 text-gray-600 text-lg max-w-2xl mx-auto">
+                        Hear what our patients say about their experience with our healthcare services.
+                    </p>
+                </div>
+
+
+
+            </div>
+        </section>
+    );
+};
+
+export default Testimonials;
