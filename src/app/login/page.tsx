c5e5066ea4053b46ec055b2809e856dcf0a4ff21
Noyon Hossain <noyonhossain560@gmail.com>
2025-05-12 13:09:08 +0600

    Implement login page navigate to homepage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -37,7 +37,7 @@ const LoginPage = () => {
                 }
 
                 setIsLoading(false);
-                // navigate.push("/");
+                navigate.push("/");
             }
             else if (!res?.success) {
                 toast.error("Login failed", {
