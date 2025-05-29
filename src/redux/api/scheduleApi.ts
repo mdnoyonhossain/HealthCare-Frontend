7aedd3db419a3f2561d2e88cad35eec2c2998956
Noyon Hossain <noyonhossain560@gmail.com>
2025-05-29 20:27:01 +0600

    Schedule Api Create

---
+++ src/redux/api/scheduleApi.ts
@@ -0,0 +1,48 @@
+import { TDoctor } from "@/types/doctor";
+import { tagTypes } from "../tagTypes";
+import baseApi from "./baseApi";
+import { TMeta } from "@/types";
+
+const scheduleApi = baseApi.injectEndpoints({
+    endpoints: (build) => ({
+        createSchedule: build.mutation({
+            query: (data) => ({
+                url: "/schedule",
+                method: "POST",
+                contentType: "application/json",
+                data
+            }),
+            invalidatesTags: [tagTypes.schedule]
+        }),
+        getAllSchedules: build.query({
+            query: (arg: Record<string, any>) => ({
+                url: "/schedule",
+                method: "GET",
+                params: arg
+            }),
+            transformResponse: (response: TDoctor[], meta: TMeta) => ({ doctors: response, meta }),
+            providesTags: [tagTypes.schedule]
+        }),
+        getSingleSchedule: build.query({
+            query: (id) => ({
+                url: `/schedule/${id}`,
+                method: "GET"
+            }),
+            providesTags: [tagTypes.schedule]
+        }),
+        deleteSchedule: build.mutation({
+            query: (id) => ({
+                url: `/schedule/${id}`,
+                method: "DELETE"
+            }),
+            invalidatesTags: [tagTypes.schedule]
+        })
+    })
+});
+
+export const {
+    useCreateScheduleMutation,
+    useGetAllSchedulesQuery,
+    useGetSingleScheduleQuery,
+    useDeleteScheduleMutation
+} = scheduleApi;
